8b54de613cd13fe547a71eb343edfab36c275380
Matthias Kausl <sig@segv.at>
2022-12-03 12:59:18 +0000

    solve 2.2

--- day2.py
+++ day2.py
@@ -6,10 +6,10 @@ ROCK = "A"
 PAPER = "B"
 SCISSORS = "C"
 # X for Rock, Y for Paper, and Z for Scissors
-#Y means you need to end the round in a draw, and Z means you need to win
-ROCK_R = "X"
-PAPER_R = "Y"
-SCISSORS_R = "Z"
+# X means you need to lose Y means you need to end the round in a draw, and Z means you need to win
+LOOSE_R = "X"
+DRAW_R = "Y"
+WIN_R = "Z"
 
 # The score for a single round is the score for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
 # plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
@@ -19,30 +19,30 @@ points = 0
 for line in Lines:
     (a, b) = line.strip().split(" ")
     if a == ROCK:
-        if b == ROCK_R:
-            points += 1+3
-        elif b == PAPER_R:
-            points += 2+6
-        elif b == SCISSORS_R:
+        if b == LOOSE_R:
             points += 3
+        elif b == WIN_R:
+            points += 2+6
+        elif b == DRAW_R:
+            points += 1+3
         else:
             raise  Exception()
     elif a == PAPER:
-        if b == ROCK_R:
+        if b == LOOSE_R:
             points += 1
-        elif b == PAPER_R:
-            points += 2+3
-        elif b == SCISSORS_R:
+        elif b == WIN_R:
             points += 3+6
+        elif b == DRAW_R:
+            points += 3+2
         else:
             raise  Exception()
     elif a == SCISSORS:
-        if b == ROCK_R:
-            points += 1+6
-        elif b == PAPER_R:
+        if b == LOOSE_R:
             points += 2
-        elif b == SCISSORS_R:
+        elif b == DRAW_R:
             points += 3+3
+        elif b == WIN_R:
+            points += 1+6
         else:
             raise  Exception()
     else:
