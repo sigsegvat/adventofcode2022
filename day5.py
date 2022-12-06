1f2102d71794d5c3918a449f580b06771918b2aa
Matthias Kausl <sig@segv.at>
2022-12-06 15:58:17 +0000

    solves 5.2

--- day5.py
+++ day5.py
@@ -18,12 +18,11 @@ print(stacks,sum(map(len,stacks)),list(map(len,stacks)))
 for line in lines[10:]:
     crates,froms,tos = [int(n) for n in re.findall(r'\d+', line)]
     print(line)
-    for i in range(crates):
-        crate = stacks[froms-1][-1]
-        print("move ",crate, stacks)
-        stacks[froms-1] = stacks[froms-1][:-1]
-        stacks[tos-1] += crate
+    crate = stacks[froms-1][-crates:]
+    print("move ",crate, stacks)
+    stacks[froms-1] = stacks[froms-1][:-crates]
+    stacks[tos-1] += crate
     
     print(stacks, sum(map(len,stacks)),list(map(len,stacks)))
 
-print(list(map(lambda s: s[-1],stacks)))
+print("".join(map(lambda s: s[-1],stacks)))
